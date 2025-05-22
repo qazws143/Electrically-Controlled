9dd6e0fab911978579079ca5deb963ddf2298e7b
qazws143 <3063941837@qq.com>
2025-05-22 21:45:20 +0800

    更新

--- HardWare/FLASH/Flash.c
+++ HardWare/FLASH/Flash.c
@@ -3,7 +3,7 @@
 #include "bsp_usart.h"
 
 // 读取指定地址的半字(16位数据)
-// faddr:读地址(此地址必须为2的倍数!!)
+// faddr:读地址
 // 返回值:对应数据.
 u16 STMFLASH_ReadHalfWord(u32 faddr)
 {
@@ -25,7 +25,7 @@ void STMFLASH_Write_NoCheck(u32 WriteAddr, u16 *pBuffer, u16 NumToWrite)
     }
 }
 // 从指定地址开始写入指定长度的数据
-// WriteAddr:起始地址(此地址必须为2的倍数!!)
+// WriteAddr:起始地址
 // pBuffer:数据指针
 // NumToWrite:半字(16位)数(就是要写入的16位数据的个数.)
 u16 STMFLASH_BUF[STM_SECTOR_SIZE / 2]; // 最多是2K字节
@@ -40,15 +40,16 @@ void STMFLASH_Write(u32 WriteAddr, u16 *pBuffer, u16 NumToWrite)
         return;                               // 非法地址
     FLASH_Unlock();                           // 解锁
     offaddr = WriteAddr - STM32_FLASH_BASE;   // 实际偏移地址.
-    secpos = offaddr / STM_SECTOR_SIZE;       // 扇区地址  0~127 for STM32F103RBT6
+    secpos = offaddr / STM_SECTOR_SIZE;       // 扇区地址
     secoff = (offaddr % STM_SECTOR_SIZE) / 2; // 在扇区内的偏移(2个字节为基本单位.)
     secremain = STM_SECTOR_SIZE / 2 - secoff; // 扇区剩余空间大小
     if (NumToWrite <= secremain)
         secremain = NumToWrite; // 不大于该扇区范围
     while (1)
-    {
-        STMFLASH_Read(secpos * STM_SECTOR_SIZE + STM32_FLASH_BASE, STMFLASH_BUF, STM_SECTOR_SIZE / 2); // 读出整个扇区的内容
-        for (i = 0; i < secremain; i++)                                                                // 校验数据
+    { // 读出整个扇区的内容
+        STMFLASH_Read(secpos * STM_SECTOR_SIZE + STM32_FLASH_BASE, STMFLASH_BUF, STM_SECTOR_SIZE / 2);
+
+        for (i = 0; i < secremain; i++) // 校验数据
         {
             if (STMFLASH_BUF[secoff + i] != 0XFFFF)
                 break; // 需要擦除
@@ -61,7 +62,8 @@ void STMFLASH_Write(u32 WriteAddr, u16 *pBuffer, u16 NumToWrite)
             {
                 STMFLASH_BUF[i + secoff] = pBuffer[i];
             }
-            STMFLASH_Write_NoCheck(secpos * STM_SECTOR_SIZE + STM32_FLASH_BASE, STMFLASH_BUF, STM_SECTOR_SIZE / 2); // 写入整个扇区
+            // 写入整个扇区
+            STMFLASH_Write_NoCheck(secpos * STM_SECTOR_SIZE + STM32_FLASH_BASE, STMFLASH_BUF, STM_SECTOR_SIZE / 2);
         }
         else
             STMFLASH_Write_NoCheck(WriteAddr, pBuffer, secremain); // 写已经擦除了的,直接写入扇区剩余区间.
